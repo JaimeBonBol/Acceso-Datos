e1fdc9c18f2a75229a60aaaf80ca9ec1e3d97663
JaimeBonBol <jaimebb2002@gmail.com>
2025-10-01 14:14:56 +0200

    Ejercicio prueba de lectura de XML en java con DOM

---
+++ UD1-Ficheros/Clase/src/XML/Books/Book.java
@@ -0,0 +1,68 @@
+package XML.Books;
+
+import java.util.ArrayList;
+
+public class Book {
+    public String id;
+    public String title;
+    public String author;
+    public ArrayList<String> authors;
+    public int year;
+    public double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public ArrayList<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(ArrayList<String> authors) {
+        this.authors = authors;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Book{id=%s, title=%s, authors=%s, year=%d, price=%.2f}",
+                id, title, getAuthors(), year, price
+        );
+    }
+}
